29bcd3646b9b073aa769d99266c876e523d450a5
parkandy4884 <143032930+parkandy4884@users.noreply.github.com>
2023-11-15 07:26:03 +0900

    part1

---
+++ UI/Menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_restart_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Maze/Maze.tscn")
+	
+	
+
+func _on_quit_pressed():
+	get_tree().quit()
